e5248b62fb4d11ffc7a25c4f756e7e4c6119cb38
Ethan Waxman <ejw99@cornell.edu>
2025-08-16 01:05:53 -0400

    Add loop

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,16 @@ fn main() {
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                println!("accepted new connection");
-                _stream.write_all(b"+PONG\r\n").unwrap();
-            }
-            Err(e) => {
-                println!("error: {}", e);
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut _stream) => {
+                    println!("accepted new connection");
+                    _stream.write_all(b"+PONG\r\n").unwrap();
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
     }
