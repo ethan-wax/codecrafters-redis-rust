3192302cb2ec667802bc725e28de521154c54389
Ethan Waxman <ejw99@cornell.edu>
2025-08-16 01:10:32 -0400

    Test GitHub sync

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,14 @@ fn main() {
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
-    loop {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(mut _stream) => {
-                    println!("accepted new connection");
-                    _stream.write_all(b"+PONG\r\n").unwrap();
-                }
-                Err(e) => {
-                    println!("error: {}", e);
-                }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut _stream) => {
+                println!("accepted new connection");
+                _stream.write_all(b"+PONG\r\n").unwrap();
+            }
+            Err(e) => {
+                println!("error: {}", e);
             }
         }
     }
