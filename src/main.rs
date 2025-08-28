ef901ccccfd429eee1a499cedca0df1b0ad2859e
Ethan Waxman <ejw99@cornell.edu>
2025-08-28 00:15:33 -0400

    Added expiry

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 #![allow(unused_imports)]
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::{result, thread};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use once_cell::sync::Lazy;
+use tokio::stream;
+use std::time::SystemTime;
 
 pub mod parse;
 use parse::{parse_command, Command};
 
-static STORE: Lazy<Arc<Mutex<HashMap<String, String>>>> = Lazy::new(|| {
+static STORE: Lazy<Arc<Mutex<HashMap<String, (String, Option<std::time::SystemTime>)>>>> = Lazy::new(|| {
     Arc::new(Mutex::new(HashMap::new()))
 });
 
@@ -50,36 +52,71 @@ fn handle_connection(mut stream: &TcpStream) {
                 let s = String::from_utf8_lossy(&buf[..n]);
 
                 match parse_command(&s).unwrap() {
-                    Command::PING => stream.write_all(b"+PONG\r\n").unwrap(),
-                    Command::ECHO(length, text) => {
-                        stream
-                            .write_all(format!("${}\r\n{}\r\n", length, text).as_bytes())
-                            .unwrap();
-                    }
-                    Command::SET(key, value) => {
-                        {
-                            let mut store_guard = STORE.lock().unwrap();
-                            store_guard.insert(key, value);
-                        }
-                        stream
-                            .write_all("+OK\r\n".as_bytes())
-                            .unwrap();
-                    }
-                    Command::GET(key) => {
-                        let value = {
-                            let store_guard = STORE.lock().unwrap();
-                            store_guard.get(&key).cloned().unwrap()
-                        };
-                        stream
-                            .write_all(format!("${}\r\n{}\r\n", value.len() as i32, value).as_bytes())
-                            .unwrap();
-                    }
+                    Command::PING => handle_ping(stream),
+                    Command::ECHO(length, text) => handle_echo(stream, &length, &text),
+                    Command::SET(key, value, exp) => handle_set(stream, &key, &value, &exp),
+                    Command::GET(key) => handle_get(stream, &key),
                 }
             }
-            Err(_) => {
-                println!("error reading into buffer");
-                break;
+            Err(e) => {
+                println!("Error: {:?}", e);
+                bulk_null(stream)
             }
         }
     }
+}
+
+fn handle_ping(mut stream: &TcpStream) {
+    stream.write_all(b"+PONG\r\n").unwrap()
+}
+
+fn handle_echo(mut stream: &TcpStream, length: &i32, text: &String) {
+    stream
+        .write_all(format!("${}\r\n{}\r\n", length, text).as_bytes())
+        .unwrap();
+}
+
+fn handle_set(mut stream: &TcpStream, key: &String, value: &String, exp: &Option<SystemTime>) {
+    {
+        let mut store_guard = STORE.lock().unwrap();
+        store_guard.insert(key.clone(), (value.clone(), exp.clone()));
+    }
+    stream
+        .write_all("+OK\r\n".as_bytes())
+        .unwrap();
+}
+
+fn handle_get(mut stream: &TcpStream, key: &String) {
+    let result = {
+        let store_guard = STORE.lock().unwrap();
+        store_guard.get(key).cloned()
+    };
+
+    match result {
+        Some((value, None)) => {
+            stream
+                .write_all(format!("${}\r\n{}\r\n", value.len() as i32, value).as_bytes())
+                .unwrap();
+        }
+        Some((value, Some(time))) => {
+            if SystemTime::now() > time {
+                {
+                    let mut store_guard = STORE.lock().unwrap();
+                    store_guard.remove(key);
+                }
+                bulk_null(stream);
+            } else {
+                stream
+                    .write_all(format!("${}\r\n{}\r\n", value.len() as i32, value).as_bytes())
+                    .unwrap();
+            }
+        }
+        None => bulk_null(stream),
+    }
+}
+
+fn bulk_null(mut stream: &TcpStream) {
+    stream
+        .write_all("$-1\r\n".as_bytes())
+        .unwrap();
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,11 @@
 use std::result::Result;
+use std::time::{Duration, SystemTime};
 
 pub enum Command {
     PING,
     ECHO(i32, String),
-    SET(String, String),
-    GET(String)
+    SET(String, String, Option<SystemTime>),
+    GET(String),
 }
 
 pub fn parse_command(command: &str) -> Result<Command, String> {
@@ -16,7 +17,10 @@ pub fn parse_command(command: &str) -> Result<Command, String> {
         .trim_start_matches("$")
         .parse::<i32>()
         .map_err(|_| "Length must be an integer".to_string())?;
-    let command_name = chunks.get(2).ok_or("Missing command".to_string())?.to_uppercase();
+    let command_name = chunks
+        .get(2)
+        .ok_or("Missing command".to_string())?
+        .to_uppercase();
 
     if command_name.len() as i32 != length {
         return Err("Actual command length doesn't match expected length".to_string());
@@ -59,12 +63,55 @@ pub fn parse_command(command: &str) -> Result<Command, String> {
                 .parse::<i32>()
                 .map_err(|_| "Val length must be an integer".to_string())?;
             let val = chunks.get(6).ok_or("Missing val".to_string())?;
-            
+
             if val.len() as i32 != val_length {
                 return Err("Actual val length doesn't match expected length".to_string());
             }
 
-            Ok(Command::SET(key.to_string(), val.to_string()))
+            Ok(Command::SET(key.to_string(), val.to_string(), None))
+        }
+        ("*5", "SET") => {
+            let key_length = chunks
+                .get(3)
+                .ok_or("Missing key length".to_string())?
+                .trim_start_matches("$")
+                .parse::<i32>()
+                .map_err(|_| "Key length must be an integer".to_string())?;
+            let key = chunks.get(4).ok_or("Missing key".to_string())?;
+
+            if key.len() as i32 != key_length {
+                return Err("Actual key length doesn't match expected length".to_string());
+            }
+
+            let val_length = chunks
+                .get(5)
+                .ok_or("Missing val length".to_string())?
+                .trim_start_matches("$")
+                .parse::<i32>()
+                .map_err(|_| "Val length must be an integer".to_string())?;
+            let val = chunks.get(6).ok_or("Missing val".to_string())?;
+
+            if val.len() as i32 != val_length {
+                return Err("Actual val length doesn't match expected length".to_string());
+            }
+
+            let expiry = chunks.get(8).ok_or("Missing exp flag")?;
+            if expiry.to_uppercase() != "PX" {
+                return Err(format!("Unrecognized flag: {}", expiry));
+            }
+
+            let millis = chunks
+                .get(10)
+                .ok_or("Missing expire time")?
+                .parse::<u64>()
+                .map_err(|_| "Expire time must be an integer")?;
+            let exp_time = SystemTime::now() + Duration::from_millis(millis);
+
+            Ok(Command::SET(
+                key.to_string(),
+                val.to_string(),
+                Some(exp_time),
+            ))
         }
         ("*2", "GET") => {
             let key_length = chunks
